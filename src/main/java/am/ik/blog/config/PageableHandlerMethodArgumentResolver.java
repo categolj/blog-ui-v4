54dc1d6158e90c37b047b9d6ff073300a89aa4ae
making <makingx@gmail.com>
2018-01-07 14:49:54 +0900

    Fix bug

--- src/main/java/am/ik/blog/config/PageableHandlerMethodArgumentResolver.java
+++ src/main/java/am/ik/blog/config/PageableHandlerMethodArgumentResolver.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.BindingContext;
 import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolverSupport;
 import org.springframework.web.server.ServerWebExchange;
@@ -41,7 +42,7 @@ public class PageableHandlerMethodArgumentResolver
 
 	private OptionalInt getInt(MultiValueMap<String, String> params, String name) {
 		String s = params.getFirst(name);
-		if (s == null) {
+		if (StringUtils.isEmpty(s)) {
 			return OptionalInt.empty();
 		}
 		try {
@@ -53,10 +54,10 @@ public class PageableHandlerMethodArgumentResolver
 	}
 
 	private int defaultPage(PageableDefault d) {
-		return d == null ? d.page() : 0;
+		return d != null ? d.page() : 0;
 	}
 
 	private int defaultSize(PageableDefault d) {
-		return d == null ? d.size() : 20;
+		return d != null ? d.size() : 20;
 	}
 }
