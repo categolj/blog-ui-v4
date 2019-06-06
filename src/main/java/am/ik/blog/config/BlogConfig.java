715ba80d7c46e8c4366db112af0f68c57f0023b6
Toshiaki Maki <tmaki@pivotal.io>
2019-06-06 12:09:42 +0900

    Bump to marked 0.11.0 (J2V8 version)

--- src/main/java/am/ik/blog/config/BlogConfig.java
+++ src/main/java/am/ik/blog/config/BlogConfig.java
@@ -1,14 +1,25 @@
 package am.ik.blog.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import am.ik.marked4j.Marked;
 import am.ik.marked4j.MarkedBuilder;
+
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.context.support.SimpleThreadScope;
 
 @Configuration
 public class BlogConfig {
 	@Bean
+	public static BeanFactoryPostProcessor beanFactoryPostProcessor() {
+		return beanFactory -> beanFactory.registerScope("thread",
+				new SimpleThreadScope());
+	}
+
+	@Bean
+	@Scope(scopeName = "thread", proxyMode = ScopedProxyMode.TARGET_CLASS)
 	public Marked marked() {
 		return new MarkedBuilder() //
 				.breaks(true) //
